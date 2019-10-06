79594666e9de567d277e99b2c58915625131319f
marshallbrain <marshalldbrain@gmail.com>
2019-10-06 15:16:25 -0400

    Initial commit

---
+++ src/test/kotlin/com/brain/pulsar/DistrictTest.kt
@@ -0,0 +1,19 @@
+package com.brain.pulsar
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class DistrictTest {
+
+	@BeforeEach
+	fun setUp() {
+		
+	}
+
+	@Test
+	fun `resource total is correct`() {
+	
+	}
+}
