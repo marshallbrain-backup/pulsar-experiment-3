c1970d9fcfb33a0681134b5e6ffb0b65848247b3
marshallbrain <marshalldbrain@gmail.com>
2019-10-06 14:01:10 -0400

    Create a map of total resources

--- src/main/kotlin/com/brain/pulsar/DistrictType.kt
+++ src/main/kotlin/com/brain/pulsar/DistrictType.kt
@@ -4,11 +4,36 @@ class DistrictType (
 		val id: String,
 		val buildType: Int = 0,
 		val cost: List<Resource> = listOf(),
-		val production: List<Resource> = listOf(),
 		val upkeep: List<Resource> = listOf(),
+		val production: List<Resource> = listOf(),
 		val employ: List<Job> = listOf()
 ){
 	
 	val resourceTotal = mutableMapOf<ResourceType, Int>()
+	
+	init {
+		
+		for (r in cost){
+			val t = resourceTotal.putIfAbsent(r.resourceType, r.amount)
+			if (t != null) {
+				resourceTotal[r.resourceType] = t + r.amount
+			}
+		}
+		
+		for (r in upkeep){
+			val t = resourceTotal.putIfAbsent(r.resourceType, r.amount)
+			if (t != null) {
+				resourceTotal[r.resourceType] = t + r.amount
+			}
+		}
+		
+		for (r in production){
+			val t = resourceTotal.putIfAbsent(r.resourceType, r.amount)
+			if (t != null) {
+				resourceTotal[r.resourceType] = t + r.amount
+			}
+		}
+		
+	}
 	
 }
