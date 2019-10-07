e01acd343dad9bc10d60ae4b47da29489f46ac8f
marshallbrain <marshalldbrain@gmail.com>
2019-10-07 09:11:11 -0400

    Create a list of empty districts

--- src/main/kotlin/com/brain/pulsar/Colony.kt
+++ src/main/kotlin/com/brain/pulsar/Colony.kt
@@ -1,4 +1,16 @@
 package com.brain.pulsar
 
-class Colony {
+class Colony (
+		val body: Body
+){
+	
+	val emptyDistrict = DistrictType("empty")
+	val districts = List(5) { _ -> emptyDistrict}
+	
+	init {
+		
+		print(districts)
+		
+	}
+
 }
