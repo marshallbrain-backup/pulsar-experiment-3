93afaf2ee0b2bdb174078f88d89021a097a99341
marshallbrain <marshalldbrain@gmail.com>
2019-10-09 14:06:17 -0400

    Restructure initialization

--- src/main/kotlin/com/brain/pulsar/colony/buildables/District.kt
+++ src/main/kotlin/com/brain/pulsar/colony/buildables/District.kt
@@ -2,14 +2,28 @@ package com.brain.pulsar.colony.buildables
 
 import com.brain.pulsar.colony.resources.ResourceType
 
-data class District (
-		var districtType: DistrictType
+data class District(
+		private val constructionQueue: ConstructionQueue
 ){
 	
 	val resourceUpkeep = mutableMapOf<ResourceType, Int>()
 	val resourceProduction = mutableMapOf<ResourceType, Int>()
 	
-	init {
+	lateinit var districtType: DistrictType
+	
+	fun queueRetool(districtType: DistrictType, instant: Boolean = true){
+		constructionQueue.add(this::retool)
+	}
+	
+	private fun retool(districtType: DistrictType){
+		this.districtType = districtType
+		initialize()
+	}
+	
+	private fun initialize() {
+		
+		resourceProduction.clear()
+		resourceUpkeep.clear()
 		
 		for (r in districtType.upkeep){
 			val t = resourceUpkeep.putIfAbsent(r.resourceType, r.amount)
