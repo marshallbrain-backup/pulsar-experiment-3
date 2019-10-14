90ef5397021ebc0b47eed257ef3534bfd85a17e6
marshallbrain <marshalldbrain@gmail.com>
2019-10-14 14:14:00 -0400

    Implement ResourceBucket

--- src/main/kotlin/com/brain/pulsar/colony/resources/ResourceBucket.kt
+++ src/main/kotlin/com/brain/pulsar/colony/resources/ResourceBucket.kt
@@ -1,4 +1,26 @@
 package com.brain.pulsar.colony.resources
 
-class ResourceBucket {
+class ResourceBucket(
+		path: String
+) {
+	
+	val bucket = mutableMapOf<String, Resource>()
+	
+	fun addResources(list: List<Resource>, type: String) {
+		for (r in list){
+			val current = bucket.putIfAbsent("$type.${r.resourceType.id}", Resource(r))
+			current?.let { it += r }
+		}
+	}
+	
+	fun modifyAmount(amount: Int) {
+		for ((_, r) in bucket) {
+			r *= amount
+		}
+	}
+	
+	fun clear(){
+		bucket.clear()
+	}
+	
 }
