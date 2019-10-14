f2c72598ab530b35ff138988f18d2161561ed83a
marshallbrain <marshalldbrain@gmail.com>
2019-10-14 14:16:17 -0400

    Add operator overloading

--- src/main/kotlin/com/brain/pulsar/colony/resources/Resource.kt
+++ src/main/kotlin/com/brain/pulsar/colony/resources/Resource.kt
@@ -4,4 +4,19 @@ data class Resource (
 		val resourceType: ResourceType,
 		var amount: Int
 ){
+	
+	constructor(r: Resource) : this(r.resourceType, r.amount) {
+	
+	}
+	
+	operator fun plusAssign(add: Resource) {
+		if (resourceType == add.resourceType) {
+			amount += add.amount
+		}
+	}
+	
+	operator fun timesAssign(multiply: Int) {
+		amount *= multiply
+	}
+	
 }
