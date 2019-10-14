ddd6c39ef814e4b95a2960a68b37cddca9fe8e63
marshallbrain <marshalldbrain@gmail.com>
2019-10-14 13:18:34 -0400

    Implement ConstructionQueue

--- src/main/kotlin/com/brain/pulsar/colony/buildables/ConstructionQueue.kt
+++ src/main/kotlin/com/brain/pulsar/colony/buildables/ConstructionQueue.kt
@@ -1,4 +1,40 @@
 package com.brain.pulsar.colony.buildables
 
 class ConstructionQueue {
+	
+	private val queue = mutableListOf<QueueEntry>()
+	
+	fun add(done: () -> Unit, time: Int) {
+		if(time == 0) {
+			done()
+		} else {
+			queue.add(QueueEntry(done, time))
+		}
+	}
+	
+	fun tick(amount: Int = 1){
+		if (queue[0].tick(amount)) {
+			queue.removeAt(0)
+		}
+	}
+	
+	fun get(){}
+	
+	fun getAll(){}
+	
+}
+
+class QueueEntry(private val done: () -> Unit, var time: Int) {
+	
+	fun tick(amount: Int) : Boolean{
+		time -= if (amount >= 0) amount else time
+		
+		if(time <= 0) {
+			done()
+			return true
+		}
+		
+		return false
+	}
+	
 }
