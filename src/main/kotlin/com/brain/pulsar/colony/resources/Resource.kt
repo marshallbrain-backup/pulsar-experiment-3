c4fd1fbd22770e150646aabe1fefd388d1a31259
marshallbrain <marshalldbrain@gmail.com>
2019-10-31 11:41:43 -0400

    Add ability to modify resource amount

--- src/main/kotlin/com/brain/pulsar/colony/resources/Resource.kt
+++ src/main/kotlin/com/brain/pulsar/colony/resources/Resource.kt
@@ -2,21 +2,26 @@ package com.brain.pulsar.colony.resources
 
 data class Resource (
 		val resourceType: ResourceType,
-		var amount: Int
+		private var baseAmount: Int
 ){
 	
-	constructor(r: Resource) : this(r.resourceType, r.amount) {
+	var amount = 0
+	var multiplyer = 0
 	
+	constructor(r: Resource) : this(r.resourceType, r.amount) {
 	}
 	
 	operator fun plusAssign(add: Resource) {
 		if (resourceType == add.resourceType) {
-			amount += add.amount
+			baseAmount += add.baseAmount
+			timesAssign(multiplyer)
 		}
 	}
 	
-	operator fun timesAssign(multiply: Int) {
-		amount *= multiply
+	operator fun timesAssign(m: Int) {
+		multiplyer = m
+		amount = baseAmount
+		amount *= multiplyer
 	}
 	
 }
