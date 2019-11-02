8a05a5f9c7bbbe4aa4829e4e452f4531d2d91c9e
marshallbrain <marshalldbrain@gmail.com>
2019-11-02 15:17:55 -0400

    Add dependency injection for EventHandler

--- src/main/kotlin/com/brain/pulsar/colony/Colony.kt
+++ src/main/kotlin/com/brain/pulsar/colony/Colony.kt
@@ -1,17 +1,19 @@
 package com.brain.pulsar.colony
 
+import com.brain.ion.handler.EventHandler
 import com.brain.pulsar.colony.buildables.DistrictType
 import com.brain.pulsar.body.Body
 import com.brain.pulsar.colony.buildables.ConstructionQueue
 import com.brain.pulsar.colony.buildables.District
 
 data class Colony (
-		val body: Body
+		val body: Body,
+		val eventHandler: EventHandler
 ){
 	
 	private val constructionQueue = ConstructionQueue()
 	private val emptyDistrict = DistrictType("empty")
-	val districts = List(5) { District(emptyDistrict, constructionQueue) }
+	val districts = List(5) { District(emptyDistrict, eventHandler, constructionQueue) }
 	
 	init {
 	}
